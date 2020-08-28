3303f6cef35b70a9edd1df352c1e7f5d7cb2f24e
azrikahar <42867097+azrikahar@users.noreply.github.com>
2020-08-28 14:37:16 +0800

    Complete commit

---
+++ components/CryptoIcon.vue
@@ -0,0 +1,40 @@
+<template functional>
+  <component :is="$options.cryptoIconComponent(props.name)" />
+</template>
+
+<script>
+const cryptoIcons = {
+  bch: require('cryptocurrency-icons/svg/color/bch.svg?inline'),
+  bnb: require('cryptocurrency-icons/svg/color/bnb.svg?inline'),
+  bsv: require('cryptocurrency-icons/svg/color/bsv.svg?inline'),
+  btc: require('cryptocurrency-icons/svg/color/btc.svg?inline'),
+  eth: require('cryptocurrency-icons/svg/color/eth.svg?inline'),
+  fsn: require('cryptocurrency-icons/svg/color/fsn.svg?inline'),
+  ftc: require('cryptocurrency-icons/svg/color/ftc.svg?inline'),
+  fuel: require('cryptocurrency-icons/svg/color/fuel.svg?inline'),
+  fun: require('cryptocurrency-icons/svg/color/fun.svg?inline'),
+  gin: require('cryptocurrency-icons/svg/color/gin.svg?inline'),
+  glxt: require('cryptocurrency-icons/svg/color/glxt.svg?inline'),
+  link: require('cryptocurrency-icons/svg/color/link.svg?inline'),
+  ltc: require('cryptocurrency-icons/svg/color/ltc.svg?inline'),
+  usdt: require('cryptocurrency-icons/svg/color/usdt.svg?inline'),
+  xrp: require('cryptocurrency-icons/svg/color/xrp.svg?inline'),
+  generic: require('cryptocurrency-icons/svg/color/generic.svg?inline'),
+}
+
+export default {
+  props: {
+    name: {
+      type: String,
+      default: () => null,
+      required: true,
+      validator(value) {
+        return Object.prototype.hasOwnProperty.call(cryptoIcons, value)
+      },
+    },
+  },
+  cryptoIconComponent(name) {
+    return name ? cryptoIcons[name] : cryptoIcons.generic
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="flex-container">
+    <div>
+      <CryptoIcon name="btc" />
+    </div>
+    <div>
+      <CryptoIcon name="eth" />
+    </div>
+    <div>
+      <CryptoIcon name="usdt" />
+    </div>
+    <div>
+      <CryptoIconMana />
+    </div>
+    <div v-for="name in iconNames" :key="`icon-${name}`">
+      <CryptoIcon :name="name" />
+    </div>
+  </div>
+</template>
+
+<script>
+import CryptoIconMana from 'cryptocurrency-icons/svg/color/mana.svg?inline'
+
+export default {
+  components: {
+    CryptoIconMana,
+  },
+  data() {
+    return {
+      iconNames: [
+        'xrp',
+        'link',
+        'bch',
+        'ltc',
+        'bsv',
+        'bnb',
+        'fsn',
+        'ftc',
+        'fuel',
+        'fun',
+        'gin',
+        'glxt',
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+.flex-container {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 1.5rem 0 1.5rem 1.5rem;
+  max-width: 48rem;
+  margin: 0 auto;
+}
+.flex-container div {
+  padding: 1rem;
+  margin: 0 1.5rem 1.5rem 0;
+  border-radius: 0.5rem;
+  background: #fff;
+  box-shadow: 0 0.325rem 1rem -0.25rem rgba(15, 25, 42, 0.1),
+    0 0.25rem 0.325rem -0.125rem rgba(15, 25, 42, 0.05);
+}
+</style>
